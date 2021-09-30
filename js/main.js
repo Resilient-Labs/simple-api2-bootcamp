048fca4082c8e9c7f2b02b3fe8d265c359ce58d2
Karim Hassan <karimhassan92@gmail.com>
2021-09-29 22:27:57 -0400

    firstcommit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.querySelector('button').addEventListener('click', getDogPic)
+
+function getDogPic(){
+  const rand = document.querySelector('input').value
+  const value = rand.toLowerCase();
+  const link = `https://dog.ceo/api/breed/${value}/images?api_key = e06db32f-3985-4131-9e27-308d4cb7f03e`
+
+  fetch(link)
+  .then(res => res.json())
+  .then(data => {
+    document.querySelector('img').src = data.message[0]
+})
+  .catch((err) => {
+    console.log(`error ${err}`)
+  })
+}
