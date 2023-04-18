96a93ca59660b67619662e26be8faa63de3a9c7a
cintnguyen <cintnguyen98@gmail.com>
2023-04-17 23:53:47 -0400

    updated CSS

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 document.querySelector(".wordSearch").addEventListener("click", getDefinition)
 
-function getDefinition(){
-    let selection = document.querySelector("input").value.toUpperCase()
+function getDefinition() {
+    let selection = document.querySelector("input").value
     fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${selection}?key=8e34da58-10bc-4c38-8e4a-9d4689835551`)
         .then((res) => res.json())
         .then((data) => {
             console.log(data)
             document.querySelector("h2").innerText = `${selection} (${data[0].fl}) - ${data[0].shortdef[0]}`
-
-
         })
-    }
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
+
+        document.querySelector("input").value = ""        
+}
