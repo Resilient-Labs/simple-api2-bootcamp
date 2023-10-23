0de470177fc5463b609a4fe4af18cd7f59b2db23
socratic617 <erik.solis02135@gmail.com>
2023-10-23 02:47:46 -0400

    fixed styling, added card tiles, cleaned up pseudo

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,40 +9,83 @@ document.querySelector("button").addEventListener("click", logHealthInfo);
 async function logHealthInfo() {
 
   try {
-      const userInput =  document.querySelector('#healthTopic').value
-      document.querySelector('.load').id = ""
-      document.querySelector('.alert').id = "hidden-alert"
-      
-      document.querySelector("#linkToArticle").innerText= "";
-      document.querySelector("#imgArticle").src = "";
-      document.querySelector("h2").innerText = "";
-      document.getElementById("demo").innerHTML = "";
+    //displaying load bar until card tiles displays
+    document.querySelector(".load").id = "";
 
-      const response = await fetch(
-        `https://health.gov/myhealthfinder/api/v3/topicsearch.json?keyword=${userInput}`
-      );
+    //checker for previous iteration of user input
+    //to make sure alert is not displayed from last user input
+    document.querySelector(".alert").id = "hidden-alert";
 
-      document.querySelector(".load").id = "hidden-load";
+    //hides template container that will be used for data
+    //this is a checker to make sure card container is not displayed from last user input
+    document.querySelector(".card-container").id = "hidden-card";
 
-      const health = await response.json();
-      console.log("Resources: ");
-      console.log(health.Result.Resources.Resource[0]);
-      document.querySelector("h2").innerText =
-        health.Result.Resources.Resource[0].Title;
-      document.querySelector("#linkToArticle").href =
-        health.Result.Resources.Resource[0].AccessibleVersion;
-      document.querySelector("#linkToArticle").innerText= "Learn More";
-      document.querySelector("#imgArticle").src =
-        health.Result.Resources.Resource[0].ImageUrl;
+    //clears all previous cards from previous input
+    document.querySelector(".card-container").innerHTML = "";
 
-      document.querySelector("#hidden-card").id = "";
+
+    //this is used to get health resource information from the api
+    const userInput = document.querySelector("#healthTopic").value;
+    const response = await fetch(
+      `https://health.gov/myhealthfinder/api/v3/topicsearch.json?keyword=${userInput}`
+    );
+    const health = await response.json();
+
+    // this is an array , used forEach() method to add card creation for all element in resoucre
+    //in other words used article to assign article content to each card
+    health.Result.Resources.Resource.forEach((article) => {
+      // to add one card
+      //img
+      let cardImg = document.createElement("img");
+      cardImg.className = "card-img-top rounded d-block";
+      cardImg.id = "imgArticle";
+      cardImg.src = article.ImageUrl;
+
+      //h4
+      let header = document.createElement("h4");
+      header.className = "card-title";
+      header.id = "title";
+      header.innerText = article.Title;
+
+      //a
+      let cardText = document.createElement("a");
+      cardText.className = "btn btn-primary";
+      cardText.id = "linkToArticle";
+      cardText.innerText = "Learn More";
+      cardText.href = article.AccessibleVersion;
+
+      //card-body
+      let cardBody = document.createElement("div");
+      cardBody.className = "card-body";
+      cardBody.appendChild(header);
+      cardBody.appendChild(cardText);
+
+      //card
+      let card = document.createElement("div");
+      card.className = "card";
+
+      //aedd img and body to card
+      card.appendChild(cardImg);
+      card.appendChild(cardBody);
+
+      // add to whole container w/ all cards
+      document.querySelector(".card-container").appendChild(card);
+    });
+
+    //this hides the load icon
+    document.querySelector(".load").id = "hidden-load";
+
+    //used to display card container after adding cards
+    document.querySelector(".card-container").id = "";
   } catch (err) {
 
     console.log("err: ");
     console.log(err.name);
     console.log(err.message);
     
-   
+    //this hides the load icon
+    document.querySelector(".load").id = "hidden-load";
+    //this displays alert
     document.querySelector('#hidden-alert').id =""
   }
 
